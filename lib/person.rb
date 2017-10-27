726c31b76124080922dfbfeeb099d02c8dc231a9
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-27 18:43:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,8 +4,8 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-
-  end 
+  # def initialize(attributes)
+  #
+  # end 
 
 end
