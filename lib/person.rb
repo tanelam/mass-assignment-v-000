f50323cbb98ed53fdb16e508009b726eedcb1215
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-27 18:48:12 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-
-  end 
+    attributes.each { |key, value| self.send(("#{key}="), value)}
+  end
 
 end
